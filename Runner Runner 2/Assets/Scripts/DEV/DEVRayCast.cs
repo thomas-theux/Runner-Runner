1e65314455e7954e9189489526167f68ab081789
Thomas Theux <t.grikschas@gmail.com>
2019-09-17 13:51:07 +0200

    Tested raycasting

---
+++ Runner Runner 2/Assets/Scripts/DEV/DEVRayCast.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DEVRayCast : MonoBehaviour {
+
+    public GameObject target;
+    private int layerMask = 1 << 9;
+
+    public Material OpaqueWall;
+    public Material TransparentWall;
+
+
+    private void Awake() {
+        // Collides with all objects except the characters
+        layerMask = ~layerMask;
+
+        // Only collides with objects that have the "Obstacles" tag
+        // layerMask = 10;
+    }
+
+    void Update() {
+        Vector3 raycastDir = target.transform.position - transform.position;
+        float maxDistance = Vector3.Distance(transform.position, target.transform.position);
+
+        RaycastHit hit;
+
+        bool hitWall = Physics.Raycast(transform.position, raycastDir, out hit, maxDistance, layerMask);
+        Debug.DrawRay(transform.position, raycastDir, Color.yellow);
+
+        if (hitWall) {
+            hit.collider.gameObject.GetComponent<Renderer>().material = TransparentWall;
+        } else {
+            hit.collider.gameObject.GetComponent<Renderer>().material = OpaqueWall;
+        }
+    }
+
+}
---
+++ Runner Runner 2/Assets/Scripts/DEV/ShowWall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowWall : MonoBehaviour {
+
+    public bool IsInvisible;
+
+    private void Update() {
+        if (IsInvisible) {
+
+        }
+    }
+
+}
